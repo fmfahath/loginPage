8648248688bedc71a3e743388862a441e481eda9
fmfahath <fmfahath.dev@gmail.com>
2024-01-16 01:26:48 +0530

    login, signup form slidimg function added+

---
+++ main.js
@@ -0,0 +1,20 @@
+let loginBtn = document.getElementById('loginBtn');
+let registerBtn = document.getElementById('registerBtn');
+let registerEl = document.getElementById('register');
+let loginEl = document.getElementById('login');
+
+
+function login(){
+    loginEl.style.left = "4px";
+    registerEl.style.right = "-520px";
+    loginBtn.classList.add('whiteBtn');
+    registerBtn.classList.remove('whiteBtn');
+}
+
+function register(){
+    loginEl.style.left = "-520px";
+    registerEl.style.right = "5px";
+    loginBtn.classList.remove('whiteBtn');
+    registerBtn.classList.add('whiteBtn');
+}
+
